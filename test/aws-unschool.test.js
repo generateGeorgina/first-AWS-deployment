af6dde132e40838f802b5e41fb85c6c7cd0c9cd8
Georgina A <appeagyeigeorgina@gmail.com>
2023-05-27 11:51:29 +0100

    Initial commit

---
+++ test/aws-unschool.test.js
@@ -0,0 +1,17 @@
+// const cdk = require('aws-cdk-lib');
+// const { Template } = require('aws-cdk-lib/assertions');
+// const AwsUnschool = require('../lib/aws-unschool-stack');
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws-unschool-stack.js
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//   // WHEN
+//   const stack = new AwsUnschool.AwsUnschoolStack(app, 'MyTestStack');
+//   // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
